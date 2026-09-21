1183bb2d0dcdb3da86ffc7fc756df853146d0571
Vitorhugov1 <vitorhugolv15@gmail.com>
2026-09-21 00:32:27 -0300

    Use transparent WP logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,5 @@
 export default function Logo() {
  return <a href="#inicio" aria-label="WP Soluções Elétricas" className="wp-logo">
-  <img src="/images/logo-wp-original.jpeg" alt="WP Soluções Elétricas: segurança, qualidade e confiança" width={1342} height={1172}/>
+  <img src="/images/logo-wp-transparent.png" alt="WP Soluções Elétricas: segurança, qualidade e confiança" width={1342} height={1172}/>
  </a>
 }
